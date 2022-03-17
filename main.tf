3270aba1ab18e0d4ca0ad3c8c497f63fbc6bbc6f
conip <51626146+conip@users.noreply.github.com>
2022-03-17 14:08:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,38 +85,34 @@ resource "azurerm_network_interface_security_group_association" "nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-resource "azurerm_virtual_machine" "instance" {
+#------------------
+resource "azurerm_linux_virtual_machine" "instance" {
   name                  = "${var.name}-srv"
   location              = var.region
   resource_group_name   = var.rg
+  size                = var.instance_size
+  admin_username      = "ubuntu"
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = var.instance_size
 
-  delete_os_disk_on_termination    = true
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16_04_0-lts-gen2"
-    version   = "latest"
+  admin_ssh_key {
+    username   = "ubuntu"
+      public_key = var.ssh_key
+    }
   }
-  storage_os_disk {
+
+  os_disk {
     name              = "${var.name}-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-  os_profile {
-    computer_name  = var.name
-    admin_username = "ubuntu"
-    #custom_data    = var.cloud_init_data
-  }
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/ubuntu/.ssh/authorized_keys"
-      key_data = var.ssh_key
-    }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 }
+
+#------------------
