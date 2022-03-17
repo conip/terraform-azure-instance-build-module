81f997390179acf042ef26a2c862d9a2f7680330
conip <51626146+conip@users.noreply.github.com>
2022-03-17 12:43:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "azurerm_virtual_machine" "instance" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
   storage_os_disk {
